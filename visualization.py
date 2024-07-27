3f9a561abf0ba0013336d476ce1c739ae4e29474
Jonathan Guevara <107374135+jonyxnx@users.noreply.github.com>
2024-07-27 12:43:19 -0600

    Update visualization.py

--- visualization.py
+++ visualization.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import plotly.graph_objects as go
-
+import numpy as np
 
 df = pd.read_json('data.json')
 df['Scores'] = pd.to_numeric(df['Scores'])
@@ -9,32 +9,44 @@ grouped_df = df.groupby('Area')
 
 fig = go.Figure()
 
-for area, group in grouped_df:
+all_scores = pd.concat([group['Scores'] for _, group in grouped_df])
+overall_mean = all_scores.mean()
+
+grouped_df_sorted = sorted(grouped_df, key=lambda x: len(x[1]), reverse=True)
+
+for area, group in grouped_df_sorted:
     fig.add_trace(go.Histogram(
         x=group['Scores'],
         name=f'Area {area}',
-        opacity=0.5,  
+        opacity=0.7,
         marker=dict(
             line=dict(
-                color='black',  
-                width=1  
+                color='black',
+                width=1
             )
         ),
         xbins=dict(
-            start=min(group['Scores'].min(), 0),  
-            end=group['Scores'].max() + 10, 
-            size=5  
+            start=min(group['Scores'].min(), 0),
+            end=group['Scores'].max() + 10,
+            size=5
         )
     ))
-    
+
+fig.add_trace(go.Scatter(
+    x=[overall_mean, overall_mean],
+    y=[0, 8000],
+    mode='lines',
+    line=dict(color='red', dash='dash'),
+    name='Media de aciertos'
+))
+
 fig.update_layout(
-    title='Histograma de Aciertos por Área',
+    title='Distribución de Aciertos por Área',
     xaxis_title='Número de Aciertos',
     yaxis_title='Frecuencia',
-    barmode='overlay',  
+    barmode='overlay',
     legend_title='Área',
-    template='plotly_white' 
+    template='plotly_white'
 )
-
 
 fig.show()
